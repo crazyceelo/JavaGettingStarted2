9a470c406362f4e9eebe4cd342c28b0e1fea9503
Carlos Perez <solracias@gmail.com>
2018-09-08 17:09:17 -0700

    java data types

---
+++ src/Animal.java
@@ -0,0 +1,25 @@
+// comment
+/* comment */
+import java.util.*;
+
+public class Animal {
+
+    public static final double FAVNUMBER = 1.6565;
+
+    private String name;
+    private int weight;
+    private boolean hasOwner = false;
+    private byte age;
+    private long uniqueId;
+    private char favoriteChar;
+    private double speed;
+    private float height;
+
+    protected static int numberOfAnimals = 0;
+
+    static Scanner userInput = new Scanner(System.in);
+
+    public Animal() {
+        super();
+    }
+}
